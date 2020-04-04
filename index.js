402d5d457b3336c7855238bd9d48e3a0cb73b3e1
Fuka <weendibol21300@gmail.com>
2020-04-04 23:46:07 +0600

    responsible for launching and importing files from the lib folder

---
+++ index.js
@@ -0,0 +1,5 @@
+// Yaml-parser
+
+module.exports.Compile      = require('./lib/compile')
+module.exports.Rules        = require('./lib/rules')
+module.exports.YamlParser   = require('./lib/yaml-parser')
